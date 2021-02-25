a5c538746b27934336b615527629d62eaadc69ff
Somal Chakraborty <somalchakraborty@Somals-MacBook-Air.local>
2021-02-25 16:40:48 +0600

    fifo indentation changes

--- Assignment4_PollingFifo/Question2/q22.cpp
+++ Assignment4_PollingFifo/Question2/q22.cpp
@@ -6,24 +6,22 @@
 #include <unistd.h> 
 
 int main(){
+    char * myfifo="xyz";
+    int fd1;
 
-char * myfifo="xyz";
-int fd1;
+    mkfifo(myfifo,0666);
 
-mkfifo(myfifo,0666);
+    char str1[100], str2[100];
 
-char str1[100], str2[100];
-
-while(1){
-    fd1=open(myfifo,O_RDONLY);
-    read(fd1,str1,100);
-    printf("User1: %s\n",str1);
-    close(fd1);
-    fd1=open(myfifo,O_WRONLY);
-    fgets(str2,100,stdin);
-    write(fd1,str2,100);
-    close(fd1);
-}
-return 0;
-
+    while(1){
+        fd1=open(myfifo,O_RDONLY);
+        read(fd1,str1,100);
+        printf("User1: %s\n",str1);
+        close(fd1);
+        fd1=open(myfifo,O_WRONLY);
+        fgets(str2,100,stdin);
+        write(fd1,str2,100);
+        close(fd1);
+    }
+    return 0;
 }
